6c5285c999caf7908b9b2b9baaa17e284e14d619
Nada00-sudo <eloukilinada2004@gmail.com>
2025-04-27 00:44:22 +0100

    changements

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const app = express()
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const PORT = process.env.PORT;
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
-//route authentification
+
 const TEST = require("./route/TEST");
 app.use("/api/TEST", TEST)
 
