244239dbbdae09d9580c8c81d16f0d1865e7b9d6
Nada00-sudo <eloukilinada2004@gmail.com>
2025-04-29 18:39:20 +0100

    Ajout des nouvelles fonctionnalités

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const app = express()
-const cookieParser = require('cookie-parser');
+//const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const PORT = process.env.PORT;
 const cors = require("cors");
 
+const corsOptions = {
+    origin:["http://localhost:4000", "http://localhost:5173"],
+    credentials: true
+}
+
 app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
+
 
 
 const TEST = require("./route/TEST");
